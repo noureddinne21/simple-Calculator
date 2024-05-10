37a71d76b6f6a28c8e8d106c06cd0d98a57be079
noureddine <noureddinne.office@gmail.com>
2024-05-10 16:34:32 +0100

    Initial commit second upload

--- app/src/main/java/com/nouroeddinne/myapplicationtesting/MainActivity.java
+++ app/src/main/java/com/nouroeddinne/myapplicationtesting/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     Button button1,button2,button3,button4;
     EditText editText1,editText2;
     TextView textView;
-    int result = 0;
+    static int result = 0;
     int num1 = 0;
     int num2 = 0 ;
 
@@ -48,8 +48,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 num1= Integer.parseInt(editText1.getText().toString());
                 num2= Integer.parseInt(editText2.getText().toString());
-                result=+num1-num2;
-                textView.setText("The Result : "+result);
+                textView.setText("The Result : "+sum(num1,num2));
                 editText1.setText("");
                 editText2.setText("");
 
@@ -86,8 +85,10 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
+    public static String sum(int n1,int n2){
+        result=+n1*n2;
+        return String.valueOf(result);
+    }
 
 
 
